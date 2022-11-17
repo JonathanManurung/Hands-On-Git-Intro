022c6505867a7919db1c12f31f3cca3a80ce2da2
JonathanManurung <118273336+JonathanManurung@users.noreply.github.com>
2022-11-18 05:55:51 +0700

    Add files via upload

---
+++ Jonathan_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Jonathan Milkyway Manurung")
+print("Jurusan: TPB Fakultas Teknik Pertambangan dan Perminyakan")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang Artificial Intelligence")
